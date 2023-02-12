8f72d5c7544f4f6fd40d62f8017f2d656c3fd876
Erudite885 <erudite885@gmail.com>
2023-02-12 10:31:01 +0100

    styling the element

--- tooltip.js
+++ tooltip.js
@@ -3,6 +3,7 @@ class Tooltip extends HTMLElement {
     super();
     this._tooltipContainer;
     this._tooltipText = "some dummy text";
+    this.attachShadow({ mode: "open" });
   }
 
   connectedCallback() {
@@ -13,13 +14,18 @@ class Tooltip extends HTMLElement {
     tooltipIcon.textContent = " (?)";
     tooltipIcon.addEventListener("mouseenter", this._showTooltip.bind(this));
     tooltipIcon.addEventListener("mouseleave", this._hideTooltip.bind(this));
-    this.appendChild(tooltipIcon);
+    this.shadowRoot.appendChild(tooltipIcon);
+    this.style.position = "relative";
   }
 
   _showTooltip() {
     this._tooltipContainer = document.createElement("div");
     this._tooltipContainer.textContent = this._tooltipText;
-    this.appendChild(this._tooltipContainer);
+    this._tooltipContainer.style.backgroundColor = "black";
+    this._tooltipContainer.style.color = "white";
+    this._tooltipContainer.style.position = "absolute";
+    this._tooltipContainer.style.zIndex = "10";
+    this.shadowRoot.appendChild(this._tooltipContainer);
   }
 
   _hideTooltip() {
